5b20180faa19caf1440152e6d493e670778dabb0
Nikita Belevich <belevish_1998@mail.ru>
2020-04-28 11:52:38 +0300

    Done Task 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 'use strict';
 
+// TODO Task 1
+const div1 = document.querySelector('.task1');
+const button1 = document.querySelector('.task1 button');
+button1.addEventListener('click', function() {this.style.display = 'none';});
+// TODO Task 1
+
+
 
